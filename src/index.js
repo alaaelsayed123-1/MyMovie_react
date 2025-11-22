81605eb380eec4c49607b5cd7b4c99375d2c3e8d
alaaelsayed123-1 <alaa.alsayed003@gmail.com>
2025-11-22 21:20:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // استبدل BrowserRouter بـ HashRouter
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { FavoritesProvider } from './contexts/FavoritesContext';
