9ef86861f5de0a17cc15cb050f7b48352617af00
alaaelsayed123-1 <alaa.alsayed003@gmail.com>
2025-11-22 21:12:19 +0200

    Switch from HashRouter to BrowserRouter
    Replaced HashRouter with BrowserRouter for routing.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; 
+import { BrowserRouter } from 'react-router-dom'; // استبدل BrowserRouter بـ HashRouter
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { FavoritesProvider } from './contexts/FavoritesContext';
@@ -8,7 +8,7 @@ import { FavoritesProvider } from './contexts/FavoritesContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AuthProvider>
         <FavoritesProvider>
           <App />
