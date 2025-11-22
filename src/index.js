14300c3316553c4235318e168bfed76995f1870c
alaaelsayed123-1 <alaa.alsayed003@gmail.com>
2025-11-22 21:13:14 +0200

    Remove comment regarding BrowserRouter replacement
    Removed comment about replacing BrowserRouter with HashRouter.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // استبدل BrowserRouter بـ HashRouter
+import { BrowserRouter } from 'react-router-dom'; 
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { FavoritesProvider } from './contexts/FavoritesContext';
