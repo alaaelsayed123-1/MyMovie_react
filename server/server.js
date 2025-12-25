ad1cd4db328c36bca2243ce726fe1f83bfce964d
alaaelsayed123-1 <alaa.alsayed003@gmail.com>
2025-12-25 13:56:26 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -35,24 +35,14 @@ db.connect((err) => {
 
 // GET MOVIES WITH OPTIONAL CATEGORY FILTER
 app.get("/movies", (req, res) => {
-  const category = req.query.category;
+  const { category } = req.query;
+  const sql = "SELECT * FROM movies" + (category && category !== "All" ? " WHERE category = ?" : "");
 
-  let sql = "SELECT * FROM movies";
-  let params = [];
-
-  if (category && category !== "All") {
-    sql += " WHERE category = ?";
-    params.push(category);
-  }
-
-  db.query(sql, params, (err, results) => {
-    if (err) {
-      console.log("SQL ERROR:", err);
-      return res.status(500).json([]);
-    }
+  db.query(sql, category && category !== "All" ? [category] : [], (err, results) => {
+    if (err) return res.status(500).json([]);
     res.json(results);
   });
-});
+});;
 
 // GET SINGLE MOVIE BY ID
 app.get("/movies/:id", (req, res) => {
