b79a6de9dfac86bd373fdf1ce56c7d6e4df62fd6
alaaelsayed123-1 <alaa.alsayed003@gmail.com>
2025-12-25 12:35:49 +0200

    Add category filtering to favorites list

--- src/pages/Ratings.js
+++ src/pages/Ratings.js
@@ -1,19 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFavorites } from '../contexts/FavoritesContext';
 import MovieCard from '../components/MovieCard';
 import '../styles/Ratings.css';
 
 const Ratings = () => {
   const { favorites } = useFavorites();
+  const [category, setCategory] = useState("All");
+
+  const filteredFavorites = favorites.filter(fav =>
+    category === "All" ? true : fav.category === category
+  );
+
+  const categories = [
+    "All",
+    "Action",
+    "Drama",
+    "Crime",
+    "Sci-Fi",
+    "Fantasy",
+    "Romance",
+    "War",
+    "Thriller",
+    "Adventure"
+  ];
 
   return (
     <div className="ratings">
-      <h1>My favorite movies</h1>
-      {favorites.length === 0 ? (
-        <p>Your list is empty. Add movies to your favorites!</p>
+      <h1>My Favorite Movies</h1>
+
+      <div className="category-dropdown">
+        <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          {categories.map(cat => (
+            <option key={cat} value={cat}>
+              {cat}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {filteredFavorites.length === 0 ? (
+        <p className="no-favorites">
+          {category === "All"
+            ? "Your list is empty. Add movies to your favorites!"
+            : `No favorite movies in "${category}" category.`}
+        </p>
       ) : (
         <div className="ratings-grid">
-          {favorites.map(movie => <MovieCard key={movie.id} movie={movie} />)}
+          {filteredFavorites.map((fav) => (
+            <MovieCard
+              key={fav.movieId}
+              movie={{
+                id: fav.movieId,
+                title: fav.title,
+                image: fav.image,
+                year: fav.year,
+                rating: fav.rating,
+                duration: fav.duration,
+                category: fav.category,
+                description: fav.description
+              }}
+            />
+          ))}
         </div>
       )}
     </div>
@@ -21,3 +71,4 @@ const Ratings = () => {
 };
 
 export default Ratings;
+
